9cadc907530ab9b85b18dc2dff8a7ab439cc9480
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-01-29 20:50:57 +0530

    Type_Conversion

---
+++ TypeConversion.py
@@ -0,0 +1,15 @@
+a="10"
+b=20
+
+c=int(a)+b
+print(c)
+
+print("Enter your name: ",end='')
+name=input()
+
+print("Enter your age: ",end='')
+age=input()
+
+print("Hello",name)
+print("You are",age,"years old.")
+
