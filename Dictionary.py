18cbc74350d90f9840ac022ad9c5ba5ec7194dd8
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-01-31 22:41:40 +0530

    DataStructures_In_Python

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+x={
+    "a":['Hello','Hi','Good Morning'],
+    "b":['Bye','Good Night'],
+    "c":16
+}
+
+y=x['a']
+y.append('Aayubowan')
+
+z=x["c"]
+z=17
+
+print(x)
---
+++ Slicing.py
@@ -0,0 +1,24 @@
+x=['a','b','c','d']
+
+y=x[2:4]
+z=x[2:]
+a=x[:2]
+
+#getting last element
+b=x[-1]
+
+#getting elements without last one
+c=x[:-1]
+
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
+
+#length a data set
+print(len(c))
+
+#String
+d="HELLO WORLD"
+print(d[6])
